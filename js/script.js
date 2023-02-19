7eeb7cab2cb902162467ecb9890540500823ff95
Azim <azimreact@gmail.com>
2023-02-20 02:57:42 +0600

    practice_work-6 - start work with DOM

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 
 1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Изменить жанр фильма, поменять "комедия" на "драма"  
 
 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+Реализовать только при помощи JS 
 
 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
 Отсортировать их по алфавиту 
@@ -23,4 +23,43 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+
+// 1) Удалить все рекламные блоки со страницы (правая часть сайта)
+// const advertisementContainer = document.querySelectorAll('.promo__adv')
+// advertisementContainer.forEach(item => {
+//     item.querySelectorAll('img').forEach(photo => {
+//         photo.remove()
+//     })
+// })
+
+const adv = document.querySelectorAll('.promo__adv img')
+adv.forEach(item => {
+    item.remove()
+})
+
+
+// 2) Изменить жанр фильма, поменять "комедия" на "драма" 
+// const genre = document.querySelector('.promo__genre') 
+// genre.replaceWith('ДРАМА')
+const poster = document.querySelector('.promo__bg')
+const genre = poster.querySelector('.promo__genre')
+genre.textContent = 'драма'
+
+// 3) Изменить задний фон постера с фильмом на изображение "bg.jpg".
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+// poster.style.cssText = 'background-image: url("img/bg.jpg")';
+
+// 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+// Отсортировать их по алфавиту 
+
+const movieList = document.querySelector('.promo__interactive-list')
+movieList.innerHTML = '';
+movieDB.movies.sort()
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${film}
+            <div class="delete"></div>
+        </li>`
+})
 
